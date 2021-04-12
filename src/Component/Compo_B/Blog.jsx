e300e6d9b6d537688fa3a8a205538825b6b62689
iamsourav37 <souravganguly108@gmail.com>
2021-04-12 11:07:16 +0530

    useEffect hooks example

--- src/Component/Compo_B/Blog.jsx
+++ src/Component/Compo_B/Blog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BlogForm from "./BlogForm";
 import BlogList from "./BlogList";
 
@@ -9,7 +9,27 @@ const divStyle = {
 };
 
 const Blog = () => {
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([
+    {
+      id: Math.random(),
+      name: "Sourav Ganguly",
+      content:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, nisi!",
+    },
+    {
+      id: Math.random(),
+      name: "Gourab Ganguly",
+      content:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, nisi!",
+    },
+  ]);
+
+  const [myHobby, setMyHobby] = useState("Coding");
+
+  useEffect(() => {
+    console.log("use effect called");
+    console.log(blogs);
+  }, [myHobby]);
 
   const onFormSubmit = (event) => {
     const name = event.target[0].value;
@@ -19,13 +39,24 @@ const Blog = () => {
   };
 
   const onDelete = (id) => {
-    console.log("given id : ", id);
-    const data = blogs.filter((value, index) => value.id !== id);
+    const data = blogs.filter((value) => value.id !== id);
     setBlogs([...data]);
   };
 
   return (
     <div className=" container-fluid bg-light p-5" style={divStyle}>
+      <div className="bg-info text-light">
+        <h2 className="display-5 p-3">
+          My hobby :{" "}
+          <span className="font-weight-bold text-warning"> {myHobby}</span>
+        </h2>
+        <button
+          className="btn btn-warning m-1"
+          onClick={() => setMyHobby("Travelling")}
+        >
+          Change Hobby
+        </button>
+      </div>
       <BlogForm onFormSubmit={onFormSubmit} />
       <br />
       <BlogList blogs={blogs} deleteHandle={onDelete} />
